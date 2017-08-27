1ae11ff439dc8564e3f297e8349c0596171bc47b
jstakun <jstakun@redhat.com>
2017-08-27 17:44:57 +0200

    removed cache bean

--- src/main/java/net/gmsworld/server/utils/memcache/CacheBean.java
+++ src/main/java/net/gmsworld/server/utils/memcache/CacheBean.java
@@ -11,37 +11,46 @@ import org.infinispan.manager.EmbeddedCacheManager;
 
 @Singleton
 public class CacheBean {
-	@Resource(lookup = "java:jboss/infinispan/gmsworld")
-	private EmbeddedCacheManager container;
-	private Cache<String, Object> cache;
+	
+   //<cache-container name="gms-world" default-cache="local" jndi-name="java:jboss/infinispan/gmsworld">
+   //    <local-cache name="local">
+    //      <expiration lifespan="360000" />
+     //  </local-cache> 
+    //</cache-container>
+	
+	//@Resource(lookup = "java:jboss/infinispan/gmsworld")
+	//private EmbeddedCacheManager container;
+	//private Cache<String, Object> cache;
 	
 	@PostConstruct
 	public void start()
     {
-		cache = container.getCache();
-		cache.addListener(new LoggingListener());
+		//cache = container.getCache();
+		//cache.addListener(new LoggingListener());
 	}
 	
 	public void put(String key, Object value) {
-		cache.put(key, value);
+		//cache.put(key, value);
 	}
 	
 	public Object getObject(String key) {
-		return cache.get(key);
+		//return cache.get(key);
+		return null;
 	}
 	
 	public void putShort(String key, Object value) {
-		cache.put(key, value, 120, TimeUnit.SECONDS);
+		//cache.put(key, value, 120, TimeUnit.SECONDS);
 	}
 	
 	public void increment(String key) {
-		Integer value = (Integer)cache.get(key);
-		if (value != null) {
-			cache.replace(key, value+1);
-		}
+		//Integer value = (Integer)cache.get(key);
+		//if (value != null) {
+		//	cache.replace(key, value+1);
+		//}
 	}
 	
 	public boolean containsKey(String key) {
-		return cache.containsKey(key);
+		//return cache.containsKey(key);
+		return false;
 	}
 }
