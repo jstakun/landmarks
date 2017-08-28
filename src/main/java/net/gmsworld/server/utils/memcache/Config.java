3eb565b004098b0472d6d1adb993dfa3525f9c5f
jstakun <jstakun@redhat.com>
2017-08-28 14:54:21 +0200

    added cache config

---
+++ src/main/java/net/gmsworld/server/utils/memcache/Config.java
@@ -0,0 +1,34 @@
+package net.gmsworld.server.utils.memcache;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+
+import org.infinispan.configuration.global.GlobalConfiguration;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.manager.EmbeddedCacheManager;
+
+public class Config {
+
+	@Produces
+	@ApplicationScoped
+	public org.infinispan.AdvancedCache<String, Object> getLocalCache() {
+		org.infinispan.Cache<String, Object> basicCache = getLocalCacheManager().getCache("gmsworld-cache",true);
+		return basicCache.getAdvancedCache();
+	}
+		
+	private EmbeddedCacheManager getLocalCacheManager() {
+		GlobalConfiguration glob = new GlobalConfigurationBuilder()
+			.globalJmxStatistics().allowDuplicateDomains(true).enable().build();
+	
+		org.infinispan.configuration.cache.Configuration loc = new org.infinispan.configuration.cache.ConfigurationBuilder()
+			.expiration().lifespan(1,TimeUnit.DAYS)
+			.build();
+		
+		return new DefaultCacheManager(glob, loc, true);
+	}
+
+}
+
