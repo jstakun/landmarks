c43e9490b590347d204b6b81dd40ec4d99d1b4be
jstakun <jstakun@redhat.com>
2017-11-27 20:40:40 +0100

    email action

---
+++ src/main/java/net/gmsworld/server/struts/MailAction.java
@@ -0,0 +1,129 @@
+package net.gmsworld.server.struts;
+
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class MailAction extends ActionSupport implements ServletRequestAware {
+
+	 private static final long serialVersionUID = 1L;
+	 private Logger logger = Logger.getLogger(getClass().getName());
+	 
+	 private HttpServletRequest request;
+		
+	 private String from;
+	    private String password;
+	    private String to;
+	    private String subject;
+	    private String body;
+
+	    static Properties properties = new Properties();
+	    static
+	    {
+	       String host = System.getenv("SMTP_HOST");
+	       String port = System.getenv("SMTP_PORT");	
+	       properties.put("mail.smtp.host", host);
+	       properties.put("mail.smtp.socketFactory.port", port);
+	       //properties.put("mail.smtp.socketFactory.class",
+	       //             "javax.net.ssl.SSLSocketFactory");
+	       properties.put("mail.smtp.auth", "true");
+	       properties.put("mail.smtp.port", port);
+	    }
+	    
+	    @Override
+		public void setServletRequest(HttpServletRequest request) {
+			this.request = request;
+			
+		}
+
+	    public String execute() 
+	    {
+	       try
+	       {
+	          Session session = Session.getDefaultInstance(properties,  
+	             new javax.mail.Authenticator() {
+	              protected PasswordAuthentication 
+	              getPasswordAuthentication() {
+	              return new 
+	              PasswordAuthentication(from, password);
+	             }});
+
+	          Message message = new MimeMessage(session);
+	          message.setFrom(new InternetAddress(from));
+	          message.setRecipients(Message.RecipientType.TO, 
+	             InternetAddress.parse(to));
+	          message.setSubject(subject);
+	          message.setText(body);
+	          Transport.send(message);
+	          String output = "{\"status\":\"Message sent to " + to + "\"}";
+	          request.setAttribute("output", output);
+	          return SUCCESS;
+	       }
+	       catch(Exception e)
+	       {
+	          logger.log(Level.SEVERE, e.getMessage(), e);
+	          addActionError(e.getMessage());
+	          return ERROR;  
+	       }
+	    }
+
+	    public String getFrom() {
+	       return from;
+	    }
+
+	    public void setFrom(String from) {
+	       this.from = from;
+	    }
+
+	    public String getPassword() {
+	       return password;
+	    }
+
+	    public void setPassword(String password) {
+	       this.password = password;
+	    }
+
+	    public String getTo() {
+	       return to;
+	    }
+
+	    public void setTo(String to) {
+	       this.to = to;
+	    }
+
+	    public String getSubject() {
+	       return subject;
+	    }
+
+	    public void setSubject(String subject) {
+	       this.subject = subject;
+	    }
+
+	    public String getBody() {
+	       return body;
+	    }
+
+	    public void setBody(String body) {
+	       this.body = body;
+	    }
+
+	    public static Properties getProperties() {
+	       return properties;
+	    }
+
+	    public static void setProperties(Properties properties) {
+	     MailAction.properties = properties;
+	    }
+	}
---
+++ src/main/webapp/email.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="s" uri="/struts-tags"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Email Form</title>
+</head>
+<body>
+   <em>The form below uses GMS World SMTP server. 
+   So you need to enter a GMS World username and password
+   </em>
+   <form action="/actions/emailer" method="post">
+   <label for="from">From</label><br/>
+   <input type="text" name="from"/><br/>
+   <label for="password">Password</label><br/>
+   <input type="password" name="password"/><br/>
+   <label for="to">To</label><br/>
+   <input type="text" name="to"/><br/>
+   <label for="subject">Subject</label><br/>
+   <input type="text" name="subject"/><br/>
+   <label for="body">Body</label><br/>
+   <input type="text" name="body"/><br/>
+   <input type="submit" value="Send Email"/>
+   </form>
+</body>
+</html>
