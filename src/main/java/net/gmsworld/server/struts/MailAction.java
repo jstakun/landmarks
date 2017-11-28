be9403903bd42fb28b453564e69326c711c5fe3d
jstakun <jstakun@redhat.com>
2017-11-28 16:56:25 +0100

    import fix

--- src/main/java/net/gmsworld/server/struts/MailAction.java
+++ src/main/java/net/gmsworld/server/struts/MailAction.java
@@ -11,7 +11,6 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeUtility;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
