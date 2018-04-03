c1c2860270286b1bbeb7986b0b34697629b1148f
jstakun <jstakun@redhat.com>
2018-04-03 17:01:43 +0200

    testing

--- src/main/java/net/gmsworld/server/struts/MailAction.java
+++ src/main/java/net/gmsworld/server/struts/MailAction.java
@@ -52,23 +52,24 @@ public class MailAction extends ActionSupport implements ServletRequestAware {
 	       String sslport = System.getenv("SMTP_SSL_PORT");
 	       if (StringUtils.isNotEmpty(sslport)) {
 	    	   Logger.getLogger("MailAction").log(Level.INFO, "Mail agent will connect to " + host + ":" + sslport);
-	    	   /*try {
+	    	   try {
 	    		   MailSSLSocketFactory socketFactory = new MailSSLSocketFactory();
 	    		   socketFactory.setTrustAllHosts(true);
-	    		   properties.put("mail.smtp.socketFactory", socketFactory);
-	    		   //properties.put("mail.smtp.ssl.socketFactory", socketFactory);
+	    		   //properties.put("mail.smtp.socketFactory", socketFactory);
+	    		   properties.put("mail.smtp.ssl.socketFactory", socketFactory);
 	    	   } catch (Exception e) {
 	    		   Logger.getLogger("MailAction").log(Level.SEVERE, e.getMessage(), e);
 	    		   //properties.put("mail.smtp.ssl.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-		       }*/
+		       }
 	    	   //properties.put("mail.smtp.socketFactory.class", "com.sun.mail.util.MailSSLSocketFactory");
     		   
-	    	   properties.put("mail.smtp.ssl.trust", "*"); //host);
+	    	   //properties.put("mail.smtp.ssl.trust", "*"); //host);
 	    	   //properties.put("mail.smtp.socketFactory.port", sslport);
 	    	   //properties.put("mail.smtp.ssl.socketFactory.port", sslport);
 	    	   properties.put("mail.smtp.port", sslport);
-	           properties.put("mail.smtp.ssl.port", sslport);
-	           properties.put("mail.smtp.ssl.enable", true);
+	           //properties.put("mail.smtp.ssl.port", sslport);
+	    	   properties.put("mail.smtp.ssl.checkserveridentity", "true");
+	           properties.put("mail.smtp.ssl.enable", "true");
 	       } else {
 	    	   Logger.getLogger("MailAction").log(Level.INFO, "Mail agent will connect to " + host + ":" + port);
 	    	   properties.put("mail.smtp.port", port);
