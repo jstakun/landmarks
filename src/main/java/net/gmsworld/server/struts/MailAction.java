3fe481b5c05412bda24756b2c65356063d910173
jstakun <jstakun@redhat.com>
2018-04-03 16:24:49 +0200

    testing

--- src/main/java/net/gmsworld/server/struts/MailAction.java
+++ src/main/java/net/gmsworld/server/struts/MailAction.java
@@ -61,12 +61,14 @@ public class MailAction extends ActionSupport implements ServletRequestAware {
 	    		   properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 	    		   properties.put("mail.smtp.ssl.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 		       }*/
-	    	   properties.put("mail.smtp.ssl.trust", "*"); //host);
+	    	   properties.put("mail.transport.protocol", "smtps");
+		       properties.put("mail.smtp.ssl.trust", "*"); //host);
 	    	   //properties.put("mail.smtp.socketFactory.port", sslport);
 	    	   //properties.put("mail.smtp.ssl.socketFactory.port", sslport);
 	    	   properties.put("mail.smtp.port", sslport);
 	           properties.put("mail.smtp.ssl.port", sslport);
 	           properties.put("mail.smtp.ssl.enable", true);
+	           properties.put("mail.smtps.ssl.enable", true);
 	       } else {
 	    	   Logger.getLogger("MailAction").log(Level.INFO, "Mail agent will connect to " + host + ":" + port);
 	    	   properties.put("mail.smtp.port", port);
